7d5445736a5eb19bcb1d8ba15cf2cb7ae4ca3b57
Andressasabrantes <andressasabrantes@gmail.com>
2023-06-29 15:06:42 -0300

    main: Creating Category Model

---
+++ RentFlixWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentFlixWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
