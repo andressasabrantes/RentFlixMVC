ad093b51f28aae66b3bc58826692b2ebb5bac148
Andressasabrantes <andressasabrantes@gmail.com>
2023-06-30 14:00:04 -0300

    main: Displaying all the categories in the view

--- RentFlixWeb/Controllers/CategoryController.cs
+++ RentFlixWeb/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace RentFlixWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
