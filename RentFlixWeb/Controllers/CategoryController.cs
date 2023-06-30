45f6751fdc5be74e82c37c7d720ad34e7b4d4d23
Andressasabrantes <andressasabrantes@gmail.com>
2023-06-30 13:46:44 -0300

    main: Getting all Categories

--- RentFlixWeb/Controllers/CategoryController.cs
+++ RentFlixWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentFlixWeb.Data;
+using RentFlixWeb.Models;
 
 namespace RentFlixWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
