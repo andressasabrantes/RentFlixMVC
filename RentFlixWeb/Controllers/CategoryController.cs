7e13ebf4c6aa286016e42798c281f15de78dd9db
Andressasabrantes <andressasabrantes@gmail.com>
2023-07-04 10:53:22 -0300

    main: Creating Create Category view

--- RentFlixWeb/Controllers/CategoryController.cs
+++ RentFlixWeb/Controllers/CategoryController.cs
@@ -16,5 +16,9 @@ namespace RentFlixWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
