e175ec70eef66646d967b16dbd640b7f68220f42
Andressasabrantes <andressasabrantes@gmail.com>
2023-07-05 13:21:27 -0300

    main: Creating Category Create Controller and connecting to db

--- RentFlixWeb/Controllers/CategoryController.cs
+++ RentFlixWeb/Controllers/CategoryController.cs
@@ -20,5 +20,12 @@ namespace RentFlixWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
