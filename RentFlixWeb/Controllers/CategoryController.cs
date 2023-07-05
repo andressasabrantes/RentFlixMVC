12fd94f2bf1ca49fdec097fd9cfddf6c8ee12c52
Andressasabrantes <andressasabrantes@gmail.com>
2023-07-05 13:38:10 -0300

    main: Adding server side validations

--- RentFlixWeb/Controllers/CategoryController.cs
+++ RentFlixWeb/Controllers/CategoryController.cs
@@ -23,9 +23,14 @@ namespace RentFlixWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
         }
     }
 }
